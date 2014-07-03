9d92723cca6a69d6819127f5e9fa0e2c3f46b1fb
marinas <marinas@tokbox.com>
2014-07-03 12:19:47 +0200

    not include talk headers

--- android/build-android.sh
+++ android/build-android.sh
@@ -104,9 +104,6 @@ for ARCH in $ARCHS; do
 	cd $LIBS_DEST
 	LIBS=`find $BASE_PATH/$BRANCH/out/$BUILD_MODE -name '*.a'`
 	for LIB in $LIBS; do
-      echo "LIBS"
-      echo "$LIB"
-      
 	    LIB_TYPE=$(get_file_type "$LIB")
 	    if is_file_type_thin_archive "$LIB_TYPE"; then
 		copy_thin $AR $LIB `pwd`
@@ -129,7 +126,7 @@ echo "WEBRTC_REVISION=$REVISION" > build.properties
 
 cp -v $BASE_PATH/$BRANCH/out/$BUILD_MODE/*.jar $LIBS_DEST
 
-HEADERS=`find webrtc third_party talk -name *.h | grep -v android_tools`
+HEADERS=`find webrtc third_party -name *.h | grep -v android_tools`
 while read -r header; do
     mkdir -p $HEADERS_DEST/`dirname $header`
     cp $header $HEADERS_DEST/`dirname $header`

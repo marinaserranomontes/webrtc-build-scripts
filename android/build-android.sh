bc037678690b0d0642ff4a6ac03af05c938c784e
marinas <marinas@tokbox.com>
2014-07-03 10:50:22 +0200

    Disable builld with libjingle

--- android/build-android.sh
+++ android/build-android.sh
@@ -85,7 +85,7 @@ for ARCH in $ARCHS; do
 
 	source build/android/envsetup.sh --target-arch=$ARCH
 
-	export GYP_DEFINES="build_with_libjingle=1 OS=android \
+	export GYP_DEFINES="build_with_libjingle=0 OS=android \
                             build_with_chromium=0 \
                             enable_tracing=1 \
                             include_tests=0 \
